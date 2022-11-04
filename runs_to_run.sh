0667dda904c376fe68c6ef335353486b4e145c0a
damaggu <markusm2342@gmail.com>
2022-11-03 20:41:43 -0700

    updated runs for testing 3

--- runs_to_run.sh
+++ runs_to_run.sh
@@ -5,29 +5,29 @@
 
 ### byol
 # training run 50 epochs
-python ./tools/run_net.py --cfg configs/mabe/ucf_BYOL_SlowR50_8x8.yaml
+python run_net.py --cfg ../configs/mabe/ucf_BYOL_SlowR50_8x8.yaml
 # eval run 50 epochs
 # supervised run 50 epochs
 # finetuning from pretrained run 50 epochs
-python ./tools/run_net.py --cfg configs/mabe/finetune_ucf_Slow_R50_syn8.yaml
+python run_net.py --cfg ../configs/mabe/finetune_ucf_Slow_R50_syn8.yaml
 # finetuning eval run from 50 epochs
 
 ### mae
 
 # training run 50 epochs
-python ./tools/run_net.py --cfg /configs/mabe/ucf_VIT_B_16x4_MAE_PT.yaml
+python run_net.py --cfg ../configs/mabe/ucf_VIT_B_16x4_MAE_PT.yaml
 # eval run 50 epochs
 # supervised run 50 epochs
 # finetuning from pretrained run 50 epochs
-python ./tools/run_net.py --cfg /configs/mabe/finetune_ucf_VIT_B_16x4_MAE_PT.yaml
+python run_net.py --cfg ../configs/mabe/finetune_ucf_VIT_B_16x4_MAE_PT.yaml
 # finetuning eval run from 50 epochs
 
 ### MaskFeat
 
 # training run 50 epochs
-python ./tools/run_net.py --cfg /configs/mabe/ucf_MVITv2_S_16x4_MaskFeat_PT.yaml
+python run_net.py --cfg ../configs/mabe/ucf_MVITv2_S_16x4_MaskFeat_PT.yaml
 # eval run 50 epochs
 # supervised run 50 epochs
 # finetuning from pretrained run 50 epochs
-python ./tools/run_net.py --cfg /configs/mabe/finetune_ucf_MVITv2_S_16x4_MaskFeat_PT.yaml
+python run_net.py --cfg ../configs/mabe/finetune_ucf_MVITv2_S_16x4_MaskFeat_PT.yaml
 # finetuning eval run from 50 epochs
