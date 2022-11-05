13d18a05e80b75f07aa74d35fcb3c4f4f2d60e78
damaggu <markusm2342@gmail.com>
2022-11-04 23:47:51 -0700

    beetle csv creator

---
+++ create_csv_for_beetle.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import json
+import os
+
+root_Path = "/root/"
+PATH_to_videos = root_path + "/root/userTrain_full_splitted/"
+PATH_to_txt_files = root_path + "ucfTrainTestlist/classInd.txt"
+
+# results paths
+PATH_to_classInd = root_Path + "classInd.json"
+PATH_to_train = root_Path + "train.csv"
+PATH_to_test = root_Path + "test.csv"
+PATH_to_val = root_Path + "val.csv"
+
+# get all video files in PATH_to_videos
+all_files = os.listdir(PATH_to_videos)
+all_files = [f for f in all_files if f.endswith(".mp4")]
+
+# resulting entries
+train = []
+for f in all_files:
+    train.append([f, 0])
+
+# to csv file
+df = pd.DataFrame(train, columns=["video", "label"], index=None, header=None, sep=" ")
+df.to_csv(PATH_to_train, index=False, header=False)
+df.to_csv(PATH_to_test, index=False, header=False)
+df.to_csv(PATH_to_val, index=False, header=False)
+
+# get classInd
+classInd = {"rnd0": 0}
+# save resulting dict as json
+with open(PATH_to_classInd, "w") as fp:
+    json.dump(classInd, fp)
