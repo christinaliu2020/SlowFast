64540aed962f68fd5fabb3468aeca240f113113c
damaggu <markusm2342@gmail.com>
2022-11-05 11:07:14 -0700

    new setup sh file

--- setup.sh
+++ setup.sh
@@ -1,17 +1,29 @@
+### setup file
+conda create -n SlowFast python=3.8 -y
+conda activate SlowFast
+conda update -n base -c defaults conda -y
 conda install -y -c conda-forge gxx=9
+#sudo apt install gcc-9
 conda install -y av -c conda-forge
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
 pip install pytorchvideo
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 git clone https://github.com/facebookresearch/detectron2 detectron2_repo
 pip install -e detectron2_repo
 
-conda update -n base -c defaults conda -y
 conda install -c conda-forge libiconv
-conda install ffmpeg=5.1 -c conda-forge -y
+conda install ffmpeg=4.2 -c conda-forge -y
 pip install numpy simplejson psutil opencv-python pillow requests urllib3 scipy pandas tqdm scikit-learn
 
+#git clone --recursive https://github.com/pytorch/pytorch
+#pip install cmake --upgrade
+#cd pytorch
+#git submodule update --init --recursive
+#python setup.py install
+
+
 #conda install pytorch torchvision torchaudio cudatoolkit=11.6 -c pytorch -c conda-forge
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+conda remove pytorch torchvision torchaudio -y
+#pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 pip install 'git+https://github.com/facebookresearch/fvcore'
 pip install -U iopath
 conda install -y -c conda-forge moviepy
@@ -20,11 +32,13 @@ pip install 'git+https://github.com/facebookresearch/fairscale'
 
 git clone https://github.com/pytorch/vision.git
 cd vision
-git checkout v0.9.0
+#git checkout v0.9.0
 python setup.py install
+cd ..
 
-pip install -U git+
+#pip install -U git+
 
 python setup.py build develop
 
+pip uninstall pytorchvideo
 pip install "git+https://github.com/facebookresearch/pytorchvideo.git"
