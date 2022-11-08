77d90ef11bcbeea8030b0d9bb7cf8bc1a034920d
damaggu <markusm2342@gmail.com>
2022-11-07 22:30:53 -0800

    runfiles per vast instance

---
+++ configs/mabe/final_runs/runfiles/3107_10984_maskratio_maskfeat_2.sh
@@ -0,0 +1,3 @@
+python run_net.py --cfg ../configs/mabe/final_runs/maskfeat/Beetle_MVITv2_S_16x4_MaskFeat_PT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/mask_ratio/Beetle_MAE_Cube_05_VIT_B_16x4_PT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/mask_ratio/Beetle_MAE_Cube_075_VIT_B_16x4_PT.yaml
---
+++ configs/mabe/final_runs/runfiles/3443_39182_beetle_maskmode.sh
@@ -0,0 +1,6 @@
+python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Beetle_MAE_Cube_09_VIT_B_16x4_PT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Beetle_MAE_Tube_09_VIT_B_16x4_PT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Beetle_MAE_Frame_075_VIT_B_16x4_PT.yaml
+
+
+
