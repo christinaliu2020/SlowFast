4af53897ccf7130aff6f9790e3c8ef917c56d943
damaggu <markusm2342@gmail.com>
2022-11-09 11:57:32 -0800

    3107 other byol runs

---
+++ tools/3107_10984_mouse_rhoByol.sh
@@ -0,0 +1,5 @@
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho1_SlowR50_8x8.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho3_SlowR50_8x8.yaml
+
+
+
