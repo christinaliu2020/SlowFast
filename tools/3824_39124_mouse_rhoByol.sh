7992530cb9bcaf886b388e95e6bdbd62de84fcd2
damaggu <markusm2342@gmail.com>
2022-11-08 21:33:22 -0800

    updated byol runfiles for 3824 on vast

--- tools/3824_39124_mouse_rhoByol.sh
+++ tools/3824_39124_mouse_rhoByol.sh
@@ -1,7 +1,7 @@
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho4_SlowR50_8x8.yaml
 python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho2_SlowR50_8x8.yaml
 python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho1_SlowR50_8x8.yaml
 python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho3_SlowR50_8x8.yaml
-python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Mouse_BYOL_rho4_SlowR50_8x8.yaml
 
 
 
