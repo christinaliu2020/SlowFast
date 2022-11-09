d391d90a664f9b650e9b70049734fc9922c77fb7
damaggu <markusm2342@gmail.com>
2022-11-09 11:38:42 -0800

    runfile change, rm existing

--- tools/3690_10936_mouse_maskmode.sh
+++ tools/3690_10936_mouse_maskmode.sh
@@ -1,3 +1,2 @@
-python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Mouse_MAE_Cube_09_VIT_B_16x4_PT.yaml
 python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Mouse_MAE_Tube_09_VIT_B_16x4_PT.yaml
 python run_net.py --cfg ../configs/mabe/final_runs/mask_strategy/Mouse_MAE_Frame_075_VIT_B_16x4_PT.yaml
