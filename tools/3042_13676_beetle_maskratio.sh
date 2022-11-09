e8b8028a45d158e59b9fa62eb6c7a63a3bd6c74c
damaggu <markusm2342@gmail.com>
2022-11-09 00:00:04 -0800

    beetle maskratio

---
+++ tools/3042_13676_beetle_maskratio.sh
@@ -0,0 +1,2 @@
+python run_net.py --cfg ../configs/mabe/final_runs/mask_ratio/Beetle_MAE_Cube_05_VIT_B_16x4_PT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/mask_ratio/Beetle_MAE_Cube_075_VIT_B_16x4_PT.yaml
