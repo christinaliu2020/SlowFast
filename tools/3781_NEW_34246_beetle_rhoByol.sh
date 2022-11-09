a9b2bd4d0949717261f81f3ee42bdcaed3223b66
damaggu <markusm2342@gmail.com>
2022-11-08 21:33:59 -0800

    new runfile for 3781

---
+++ tools/3781_NEW_34246_beetle_rhoByol.sh
@@ -0,0 +1,5 @@
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Beetle_BYOL_rho2_SlowR50_8x8.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Beetle_BYOL_rho1_SlowR50_8x8.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/byol_rho/Beetle_BYOL_rho3_SlowR50_8x8.yaml
+
+
