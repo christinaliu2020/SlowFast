ff64a2d5ffbc0c0e36334f5fd0c32cca84f2c1ee
damaggu <markusm2342@gmail.com>
2022-11-09 22:32:24 -0800

    new cfg mouse file

---
+++ tools/3824_39124_mouse_pretrained_eval.sh
@@ -0,0 +1,3 @@
+python run_net.py --cfg ../configs/mabe/final_runs/evaluation_runs/pretrained_models/mouse_embed_k400_Slow_8x8_R50_syn8.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/evaluation_runs/pretrained_models/mouse_embed_k400_VIT_B_16x4_FT.yaml
+python run_net.py --cfg ../configs/mabe/final_runs/evaluation_runs/pretrained_models/mouse_embed_k400_MVITv2_S_16x4_FT.yaml
