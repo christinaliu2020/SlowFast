9e21c74c586dafeb730799d050e0deb604b4e934
damaggu <markusm2342@gmail.com>
2022-11-09 21:16:09 -0800

    updated pretrainecd models getter

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -1,6 +1,6 @@
 # get MaskFeat pretrained model
-wget https://dl.fbaipublicfiles.com/pyslowfast/masked_models/k400_MVIT_S_MaskFeat_PT_epoch_00300.pyth -P ./pretrained_models/
+wget https://dl.fbaipublicfiles.com/pyslowfast/masked_models/k400_MVIT_S_MaskFeat_PT_epoch_00300.pyth -P /root/pretrained_models/
 # get MAE pretrained model
-wget https://dl.fbaipublicfiles.com/pyslowfast/masked_models/VIT_B_16x4_MAE_PT.pyth -P ./pretrained_models/
+wget https://dl.fbaipublicfiles.com/pyslowfast/masked_models/VIT_B_16x4_MAE_PT.pyth -P /root/pretrained_models/
 # get BYOL pretrained model
-wget https://dl.fbaipublicfiles.com/pyslowfast/videomoco_models/BYOL_SlowR50_8x8_T4_epoch_00200.pyth -P ./pretrained_models/
+wget https://dl.fbaipublicfiles.com/pyslowfast/videomoco_models/BYOL_SlowR50_8x8_T4_epoch_00200.pyth -P /root/pretrained_models/
