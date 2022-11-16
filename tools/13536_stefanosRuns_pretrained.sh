f237a2ce6b651df918119a323c94e4f63d1ea960
damaggu <markusm2342@gmail.com>
2022-11-16 10:08:38 -0800

    bs , num gpus, runs etc

--- tools/13536_stefanosRuns_pretrained.sh
+++ tools/13536_stefanosRuns_pretrained.sh
@@ -1,2 +1,3 @@
 python run_net.py --cfg ../configs/mabe/stefanos/mouse_embed_k400_Slow_8x8_R50_syn8.yaml
 python run_net.py --cfg ../configs/mabe/stefanos/mouse_embed_k400_VIT_B_16x4_FT.yaml
+python run_net.py --cfg ../configs/mabe/stefanos/training_Mouse_MAE_Frame_09_VIT_B_16x4_PT.yaml
